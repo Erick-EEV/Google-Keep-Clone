e4dcbb8f42269257ea2e0f978fe201db303f782d
Erick-EEV <EEVERICKEEV@gmail.com>
2021-02-24 17:12:14 -0500

    Add notes and display notes

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 class App {
     constructor(){
-       this.$form = document.querySelector('form');
+        this.note = [];
+
+        this.$placeholder = document.querySelector('#placeholder')
+       this.$form = document.querySelector('#form');
+       this.$noteTitle = document.querySelector('#note-title')
+       this.$noteText = document.querySelector('#note-text')
+       this.$formButtons = document.querySelector('#form-buttons')
        this.addEventListeners();
     }
 
     addEventListeners() {
         document.body.addEventListener('click', event => {
-            handleFormClick(event)
+            this.handleFormClick(event)
+        })
+
+        this.$form.addEventListener('submit', event => {
+            event.preventDefault();
+            const title = this.$noteTitle.value;
+            const text = this.$noteText.value;
+            const hasNote = title || text;
+            if (hasNote){
+                //addNote
+                this.addNote({title, text});
+            } else {
+                //dont add 
+            }
         })
     }
 
@@ -14,13 +33,49 @@ class App {
         const isFormClicked = this.$form.contains(event.target);
         if (isFormClicked) {
             //open form
+            this.openForm()
         } else {
+            this.closeForm()
             //clicked form
         }
     }
 
     openForm() {
         this.$form.classList.add('form-open');
+        this.$noteTitle.style.display = "block";
+        this.$formButtons.style.display = "block"
+    }
+
+    closeForm() {
+        this.$form.classList.remove('form-open');
+        this.$noteTitle.style.display = "none";
+        this.$formButtons.style.display = "none";
+    }
+    addNote(note){
+        const newNote = {
+            title: note.title,
+            text: note.text,
+            color: 'white', 
+            id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
+        };
+        this.notes = [...this.notes, newNote]
+        this.displayNotes()
+    }
+
+    displayNotes() {
+        const hasNotes = this.notes.length > 0
+        this.$placeholder.style.display = hasNotes ? 'none' : 'flex';
+        this.notes.map(note => `
+        <div style="background: ${note.color};" class="note"></div>
+        <div class="${note.title && 'note-title'}">${note.title}</div>
+        <div class="note-text">${note.text}</div>
+        <div class="toolbar-container">
+        <div class="toolbar">
+        <img class="toolbar-color" src="https://icon.now.sh/palette">
+        <img class="toolbar-delete" src="https://icon.now.sh/delete">
+        </div>
+        </div>
+        `)
     }
 }
 
